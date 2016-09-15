efd587e7cd33ec0b8ef2a8a325b4c5965ef41dbe
Kyle Correll <krcorrell2014@hotmail.com>
2016-09-15 04:42:19 +0000

    What I have thus far

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlMain = require('../controllers/others');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlMain.about);
 
 module.exports = router;
